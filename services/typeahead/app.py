e9443fca1f2f809c0d2fc15665256e8c97ddec14
Max Buck <mbuck@covermymeds.com>
2020-05-09 20:03:00 -0400

    Let's give it a go

--- services/typeahead/app.py
+++ services/typeahead/app.py
@@ -18,6 +18,21 @@ app = Flask(__name__)
 r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
 
+def spotify_search_response_to_universal_format(resp_dict):
+    results = []
+    for track_dict in resp_dict["tracks"]["items"]:
+        result_dict = {}
+        result_dict["name"] = track_dict["name"]
+        result_dict["service"] = "spotify"
+        result_dict["type"] = track_dict["type"]
+        result_dict["uri"] = track_dict["uri"]
+        result_dict["id"] = track_dict["id"]
+        result_dict["album_art"] = track_dict["album"]["images"][0]["url"]
+        result_dict["artists"] = [a["name"] for a in track_dict["artists"]]
+        results.append(result_dict)
+    return result_dict
+
+
 def spotify_search(request):
     q = request.args["q"]
     types = "track"
@@ -28,13 +43,14 @@ def spotify_search(request):
         "https://api.spotify.com/v1/search", params={"q": q, "type": types},
     )
     r.set(f"spotify_{q}_{types}", json.dumps(resp.json()), ex=60 * 60 * 24 * 7)
-    return jsonify(resp.json())
+    formatted_dict = spotify_search_response_to_universal_format(resp.json())
+    return jsonify(formatted_dict)
 
 
 @app.route("/", methods=["GET"])
 def search():
-    return getattr(current_module, f"{request.args.get('service')}_search")(request)
+    return getattr(current_module, f"{request.args['service']}_search")(request)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=6000, debug=True)
