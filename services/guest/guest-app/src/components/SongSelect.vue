f620411ac27f7e558e263ce88a3198b30b1ee711
Max Buck <mbuck@covermymeds.com>
2020-05-15 15:17:11 -0400

    It works. basic, and buggy, but it works

--- services/guest/guest-app/src/components/SongSelect.vue
+++ services/guest/guest-app/src/components/SongSelect.vue
@@ -1,19 +1,36 @@
 <template>
-  <div id="main">
+  <div id="main" class="container">
     <h1>hello</h1>
     <VueBootstrapTypeahead
       :data="songs"
       v-model="songSearch"
       size="lg"
-      :serializer="s => s.name"
-      placeholder="Type an address..."
-      @hit="selectedAddress = $event"
-    />
+      :serializer="songSerializer"
+      placeholder="Search songs or artists"
+      @hit="selectedSong = $event"
+    >
+      <template slot="append">
+        <button @click="queueSong" class="btn btn-primary">queue this song</button>
+      </template>
+      <!-- Begin custom suggestion slot -->
+      <template slot="suggestion" slot-scope="{ data, htmlText }">
+        <div class="d-flex align-items-center">
+          <img class="rounded-circle" :src="data.album_art" style="width: 40px; height: 40px;" />
+
+          <!-- Note: the v-html binding is used, as htmlText contains
+          the suggestion text highlighted with <strong> tags-->
+          <span class="ml-4" v-html="htmlText"></span>
+          <i class="ml-auto fab fa-github-square fa-2x"></i>
+        </div>
+      </template>
+    </VueBootstrapTypeahead>
   </div>
 </template>
 
+
 <script>
-const API_URL = `http://localhost:5001?service=:service&q=:q`;
+const TYPEAHEAD_URL = `http://localhost:5001?service=:service&q=:q`;
+const PLAYER_URL = "http://localhost:5002";
 import _ from "underscore";
 import VueBootstrapTypeahead from "vue-bootstrap-typeahead";
 export default {
@@ -33,11 +50,33 @@ export default {
     };
   },
   methods: {
+    queueSong: function() {
+      let url = PLAYER_URL;
+      let that = this;
+      return this.$http
+        .post(url, {
+          service: that.service,
+          uri: that.selectedSong.uri,
+          room_code: that.roomCode
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    songSerializer: function(song) {
+      return `${song.name} - ${song.artists.join(", ")}`;
+    },
     getSongs: function(query) {
       //const res = await fetch(
       //  API_URL.replace(":q", query).replace(":service", this.service)
       //);
-      let url = API_URL.replace(":q", query).replace(":service", this.service);
+      let url = TYPEAHEAD_URL.replace(":q", query).replace(
+        ":service",
+        this.service
+      );
       let that = this;
       return this.$http
         .get(url)
@@ -46,14 +85,18 @@ export default {
           that.songs = response.data;
         })
         .catch(function(error) {
+          console.log("error");
           console.log(error);
+          console.log("error");
           that.$emit("deleteCookies");
         });
     }
   },
   watch: {
-    songSearch: _.debounce(function(addr) {
-      this.getSongs(addr);
+    songSearch: _.debounce(function(query) {
+      if (query) {
+        this.getSongs(query);
+      }
     }, 500)
   }
 };
@@ -62,37 +105,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Permanent+Marker&display=swap");
-h1 {
-  color: #ffffff;
-}
-input,
-label {
+#main {
   font-family: "Luckiest Guy", cursive;
   font-size: xx-large;
 }
-input {
-  padding: 20px 12px;
-  background-color: #e0e0e0;
-  color: #000;
-  border: 0 solid;
-  border-radius: 12px;
-  box-shadow: none;
-}
-button,
-button:hover,
-button:visited,
-button:active {
-  margin-top: 20px;
-  font-family: "Luckiest Guy", cursive;
-  font-size: xx-large;
-
-  background-color: #553c7b;
-  color: #ffffff;
-  border: 0 solid;
-  border-radius: 12px;
-  box-shadow: none;
-}
-.roomCodeForm {
-  margin-top: 150px;
+a #main {
+  font-family: "Arial", cursive;
 }
 </style>
--- services/player/app.py
+++ services/player/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, jsonify
 from flask import redirect, make_response
+from flask_cors import CORS
 from functools import wraps
 import async_messenger
 import redis
@@ -12,6 +13,7 @@ import uuid
 current_module = sys.modules[__name__]
 
 app = Flask(__name__)
+CORS(app)
 
 r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
