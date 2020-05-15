afd8f1d0c2fbf14ec4a5bd2a40729388b2c9b062
Max Buck <mbuck@covermymeds.com>
2020-05-15 15:30:37 -0400

    ui improvements

--- services/guest/guest-app/src/components/SongSelect.vue
+++ services/guest/guest-app/src/components/SongSelect.vue
@@ -1,5 +1,45 @@
 <template>
   <div id="main" class="container">
+    <div class="fixed-top">
+      <nav class="navbar navbar-dark" style="background-color:#553C7B">
+        <h1>
+          <svg
+            class="bi bi-speaker"
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M9 4a1 1 0 11-2 0 1 1 0 012 0zm-2.5 6.5a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+            <path
+              fill-rule="evenodd"
+              d="M4 0a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V2a2 2 0 00-2-2H4zm6 4a2 2 0 11-4 0 2 2 0 014 0zM8 7a3.5 3.5 0 100 7 3.5 3.5 0 000-7z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </h1>
+        <img src="../assets/earbudclubimage.png" height="80px;" />
+        <h1>
+          <svg
+            class="bi bi-speaker"
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M9 4a1 1 0 11-2 0 1 1 0 012 0zm-2.5 6.5a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+            <path
+              fill-rule="evenodd"
+              d="M4 0a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V2a2 2 0 00-2-2H4zm6 4a2 2 0 11-4 0 2 2 0 014 0zM8 7a3.5 3.5 0 100 7 3.5 3.5 0 000-7z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </h1>
+      </nav>
+    </div>
+
     <h1>hello</h1>
     <VueBootstrapTypeahead
       :data="songs"
@@ -9,9 +49,6 @@
       placeholder="Search songs or artists"
       @hit="selectedSong = $event"
     >
-      <template slot="append">
-        <button @click="queueSong" class="btn btn-primary">queue this song</button>
-      </template>
       <!-- Begin custom suggestion slot -->
       <template slot="suggestion" slot-scope="{ data, htmlText }">
         <div class="d-flex align-items-center">
@@ -24,6 +61,7 @@
         </div>
       </template>
     </VueBootstrapTypeahead>
+    <button type="button" class="btn btn-primary col-sm-12" v-on:click="queueSong">QUEUE THIS SONG</button>
   </div>
 </template>
 
@@ -105,11 +143,28 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Permanent+Marker&display=swap");
-#main {
+h1 {
+  color: #ffffff;
+}
+button {
   font-family: "Luckiest Guy", cursive;
   font-size: xx-large;
 }
-a #main {
-  font-family: "Arial", cursive;
+a {
+  font-family: "Arial", sans-serif;
+}
+button,
+button:hover,
+button:visited,
+button:active {
+  margin-top: 20px;
+  font-family: "Luckiest Guy", cursive;
+  font-size: xx-large;
+
+  background-color: #553c7b;
+  color: #ffffff;
+  border: 0 solid;
+  border-radius: 12px;
+  box-shadow: none;
 }
 </style>
