1d29431a216525d50e4e7ca78ef1784be1b49cc2
Max Buck <mbuck@covermymeds.com>
2020-05-30 19:32:09 -0400

    kind of worked last time. break it again?

--- services/websocket_server/app.py
+++ services/websocket_server/app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 from flask_cors import CORS
 
 
-# from flask_socketio import SocketIO, join_room, leave_room, send, emit
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
+
+# from flask_socketio import SocketIO, send
 import os
 
 app = Flask(__name__)
@@ -13,20 +14,20 @@ socketio = SocketIO(app)
 # socketio = SocketIO(app, message_queue="redis://redis:6379/")
 
 
-# @socketio.on("join")
-# def on_join(data):
-#    username = data["username"]
-#    room = data["room"]
-#    join_room(room)
-#    send(username + " has entered the room.", room=room)
-#
-#
-# @socketio.on("leave")
-# def on_leave(data):
-#    username = data["username"]
-#    room = data["room"]
-#    leave_room(room)
-#    send(username + " has left the room.", room=room)
+@socketio.on("join")
+def on_join(data):
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+    send(username + " has entered the room.", room=room)
+
+
+@socketio.on("leave")
+def on_leave(data):
+    username = data["username"]
+    room = data["room"]
+    leave_room(room)
+    send(username + " has left the room.", room=room)
 
 
 @app.route("/socket.io/")
