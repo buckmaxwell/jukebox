8090af545950cf9b50cdedb8d82299df1acc7b5b
Max Buck <mbuck@covermymeds.com>
2020-05-30 19:18:14 -0400

    could it be so simple

--- services/websocket_server/app.py
+++ services/websocket_server/app.py
@@ -29,6 +29,16 @@ socketio = SocketIO(app)
 #    send(username + " has left the room.", room=room)
 
 
+@app.route("/socket.io")
+def socket_home():
+    return "socket home"
+
+
+@app.route("/")
+def index():
+    return "index"
+
+
 @socketio.on("message")
 def handleMessage(msg):
     print("Message: " + msg)
