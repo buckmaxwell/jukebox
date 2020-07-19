58eb05e080dcf6a29fe7732455d5143adad9876e
Max Buck <mbuck@covermymeds.com>
2020-07-19 15:10:21 -0400

    very basic selenium test

---
+++ test.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+chrome = webdriver.Remote(
+    command_executor="http://localhost:4444/wd/hub",
+    desired_capabilities=DesiredCapabilities.CHROME,
+)
+# firefox = webdriver.Remote(
+#    command_executor="http://localhost:4444/wd/hub",
+#    desired_capabilities=DesiredCapabilities.FIREFOX,
+# )
+
+chrome.get("https://www.google.com")
+print(chrome.title)
+
+# firefox.get("https://www.google.com")
+# print(firefox.title)
+
+chrome.quit()
+# firefox.quit()
+
